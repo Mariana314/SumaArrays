a26d99d6868c9b62e7f570c2c7c1f9f00d7028fe
karinatabares1 <marianaocampomedina@gmail.com>
2024-09-05 21:14:44 -0500

    Suma Arrays

--- SumaArreglos/Ejercicio2.java
+++ SumaArreglos/Ejercicio2.java
@@ -3,8 +3,19 @@ package SumaArreglos;
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		  int[] array1 = {1, 2, 3};
+	        int[] array2 = {4, 5, 6};
+	        int[] sumArray = new int[array1.length];
 
+	        for (int i = 0; i < array1.length; i++) {
+	            sumArray[i] = array1[i] + array2[i];
+	        }
+
+	        System.out.print("Arreglo resultante: ");
+	        for (int i = 0; i < sumArray.length; i++) {
+	            System.out.print(sumArray[i] + " ");
+	        }
+	        System.out.println();
 	}
 
 }
--- SumaArreglos/Ejercicio3.java
+++ SumaArreglos/Ejercicio3.java
@@ -3,8 +3,22 @@ package SumaArreglos;
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    int[] array1 = {10, 20, 30};
+        int[] array2 = {1, 2, 3};
+        int[] sumArray = new int[array1.length];
+        int totalSum = 0;
 
+        for (int i = 0; i < array1.length; i++) {
+            sumArray[i] = array1[i] + array2[i];
+            totalSum += sumArray[i];
+        }
+
+        System.out.print("Arreglo resultante: ");
+        for (int i = 0; i < sumArray.length; i++) {
+            System.out.print(sumArray[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Suma total de los elementos del arreglo resultante: " + totalSum);
 	}
 
 }
